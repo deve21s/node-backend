27176b6804b9b47d84cafedd71df3af2525323b7
deven <panchaldevendra522@gmail.com>
2021-01-25 18:53:39 +0530

    first commit

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.use(
 
  
 
-// const dburl = process.env.dburl
+const dburl = process.env.dburl
 
-mongoose.connect(process.env.dburl,{ useNewUrlParser: true,  useUnifiedTopology: true  })
-    .then(() => app.listen(process.env.PORT || 3000, () => {
+mongoose.connect(dburl,{ useNewUrlParser: true,  useUnifiedTopology: true  })
+    .then(() => app.listen(process.env.PORT || 5000, () => {
         console.log("server is started and data base is connected")
     }))
     .catch((err)=> console.log(err))
